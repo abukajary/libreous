8152d168fa7b6e52f7fd9cd0ee0760d11d6fa532
abukajary <70243848+abukajary@users.noreply.github.com>
2021-03-01 17:50:41 +0600

    redesign + shopping cart

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//SLIDER
 new Swiper('.swiper-container', {
     navigation: {
         nextEl: '.swiper-button-next',
@@ -11,22 +12,49 @@ new Swiper('.swiper-container', {
         clickable: true,
     },
     breakpoints: {
-        100: {
+        320: {
             slidesPerView: 2,
-            spaceBetween: 5,
+            spaceBetween: 10,
         },
-        600: {
+        426: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
             slidesPerView: 3,
-            spaceBetween: 5,
+            spaceBetween: 40,
         },
-        880: {
-            slidesPerView: 4,
-            spaceBetween: 15,
+        1024: {
+            slidesPerView:4,
+            spaceBetween: 50,
         },
-        1220: {
+        1280: {
             slidesPerView: 6,
-            spaceBetween: 15,
-        }
+            spaceBetween: 30,
+        },
     },
     freeMode: true,
 });
+//SLIDER
+//SHOW PROFILE
+let profileBtn = document.getElementById('profileBtn')
+let profile = document.querySelector('.profile')
+let close = document.getElementById('close')
+let isProfile = false
+profileBtn.addEventListener('click', ()=> {
+    if (isProfile) {
+        profile.style.width = "0px";
+        document.body.style.overflow = "visible";
+        profileBtn.style.color = "white";
+        isProfile = false;
+    } else {
+        profile.style.width = "30vw";
+        document.body.style.overflow = "hidden";
+        profileBtn.style.color = "#e68836";
+        isProfile = true;
+    }
+})
+close.addEventListener('click', ()=> {
+    profile.style.width = "0px";
+    document.body.style.overflow = "visible";
+})
